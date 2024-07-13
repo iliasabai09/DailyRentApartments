11fab50dd64c19ccf3508d2a77d818b230100350
iliasabai123 <ilias.abai.alser@gmail.com>
2024-07-14 02:06:09 +0500

    Проверка динамичной страницы

---
+++ pages/dynamic/[id].vue
@@ -0,0 +1,15 @@
+<template>
+<pre>{{data}}</pre>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute();
+const {data} = useFetch('https://jsonplaceholder.typicode.com/todos/' + route.params.id)
+
+</script>
+
+<style scoped>
+
+</style>
