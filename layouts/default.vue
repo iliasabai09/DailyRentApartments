7a6d499f196e55c1b89ae71918f249839d9e860c
iliasabai123 <ilias.abai.alser@gmail.com>
2024-08-08 19:54:10 +0500

    feat/делал базовую архитектуру

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>Its header</header>
+    <NuxtPage/>
+    <footer>its footer</footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>This is main page</h1>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
