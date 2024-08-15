d454c88a3479360f08166c5e59968e2307c72973
iliasabai123 <ilias.abai.alser@gmail.com>
2024-08-15 23:30:09 +0500

    develop/добавил основные библиотечные компоненты

---
+++ components/libs/LButtonFilled.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "LButtonFilled"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/libs/LFabButton.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "LFabButton"
+}
+</script>
+
+<style scoped>
+
+</style>
