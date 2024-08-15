2a02b90883846758e6469a593b8092710a38fe26
iliasabai123 <ilias.abai.alser@gmail.com>
2024-08-16 00:02:50 +0500

    feat/добавил билиотеку иконок

---
+++ components/libs/LIcon.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <component
+        :is="icons[icon]"
+        :style="{
+          width:scale + 'px',
+          height: scale + 'px',
+          color: color
+        }"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent, defineProps, type PropType } from 'vue'
+import { type IconTypes } from '../../shared/types/libs/iconsTypes.enum'
+
+type IconTypes = 'menu' | 'close';
+defineProps({
+  icon: {
+    type: String as PropType<IconTypes>,
+    required: true
+  },
+  scale: {
+    type: Number,
+    default: 20,
+    required: false
+  },
+  color: {
+    type: String,
+    required: false,
+    default: '#000'
+  }
+})
+
+const icons = {
+  menu: defineAsyncComponent(() => import('../svgs/icons/IconMenu.vue')),
+  close: defineAsyncComponent(() => import('../svgs/icons/IconClose.vue'))
+}
+
+</script>
+
+<style scoped>
+* {
+  fill: red;
+}
+
+svg path {
+  fill: red;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <h1 class="test">Ersin</h1>
+  <LIcon :icon="'menu'" scale="18"/>
+  <LIcon :icon="'close'" scale="18"/>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LIcon from '../components/libs/LIcon.vue'</script>
 
 <style scoped lang="scss">
 
