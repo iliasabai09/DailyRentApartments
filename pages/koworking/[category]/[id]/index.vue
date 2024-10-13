29144947ea64fafaf69410f5c97f72613b21fd55
iliasabai123 <ilias.abai.alser@gmail.com>
2024-10-14 00:55:54 +0500

    feat/сделал activeState для деталки

--- pages/koworking/[category]/[id]/index.vue
+++ pages/koworking/[category]/[id]/index.vue
@@ -1,10 +1,59 @@
 <template>
-  <KoworkingDetailModule/>
+  <div class="container">
+    <div class="detail">
+      <h1 class="THeadMedium detail-title">Коворкинг центр Токио {{ activeTab === 'negotiations' }}</h1>
+      <div class="line"></div>
+      <KoworkingDetail/>
+      <div class="line"></div>
+      <div class="detail-tabs">
+        <koworking-place-tab type="workSpace" :active="activeTab === 'workSpaces'"/>
+        <koworking-place-tab type="conference" :active="activeTab === 'conference'"/>
+        <koworking-place-tab type="office" :active="activeTab === 'offices'"/>
+        <koworking-place-tab type="negotiation" :active="activeTab === 'negotiations'"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import KoworkingDetailModule from '@/modules/KoworkingDetail/KoworkingDetailModule.vue'</script>
+import KoworkingDetail from './components/KoworkingDetail.vue'
+import KoworkingPlaceTab from './components/KoworkingPlaceTab.vue'
+import { ref } from 'vue'
+import { useRoute } from 'nuxt/app'
+
+const route = useRoute()
+
+const activeTab = ref(route.params.category)
+
+</script>
 
 <style scoped lang="scss">
+.detail {
+  width: 100%;
+  padding: 16px;
+  border-radius: 16px;
+  background-color: #fff;
+  margin-top: 106px;
 
+  &-title {
+    display: flex;
+    align-items: center;
+  }
+
+  &-tabs {
+    display: flex;
+    gap: 4px;
+
+    div {
+      flex: 1;
+    }
+  }
+}
+
+.line {
+  width: 100%;
+  height: 1px;
+  background-color: #D9D9D9;
+  margin: 16px 0;
+}
 </style>
